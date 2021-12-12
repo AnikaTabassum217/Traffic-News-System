b652ddc7b2e49701a62b64561b2e4310810642ed
AnikaTabassum217 <anika.tabassum579@gmail.com>
2021-12-12 22:11:39 +0600

    first commit

---
+++ src/main/java/com/example/trafficnewssystem/TrafficNewsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficnewssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficNewsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficNewsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trafficnewssystem/TrafficNewsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficnewssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficNewsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
